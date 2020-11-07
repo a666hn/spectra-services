47b666ccce71f9a9da95d107df751ed0fa07db23
Adi Hermawan <adidot78@gmail.com>
2020-11-08 01:16:55 +0700

    Update client services

--- core/client/client.go
+++ core/client/client.go
@@ -1,1 +1,47 @@
 package client
+
+import (
+	"sync"
+	"time"
+
+	configFile "github.com/skinnyguy/spectra-services/core/config"
+	service "github.com/skinnyguy/spectra-services/core/proto"
+
+	"github.com/micro/go-micro/client"
+	grpcclient "github.com/micro/go-micro/client/grpc"
+)
+
+const GoMicroPrefix string = "go.micro.srv."
+
+const (
+	// Account service ...
+	AccountService = GoMicroPrefix + "account.service"
+)
+
+func getClientOptions() []client.Option {
+	config := configFile.GetSpectraConfig()
+	options := []client.Option{
+		client.RequestTimeout(config.GRPCTimeout * time.Second),
+	}
+
+	return options
+}
+
+/* ========================== */
+// Define all client services //
+/* ========================== */
+
+var (
+	accountOnce    sync.Once
+	accountService service.AccountServicesService
+)
+
+// GetAccountService ...
+func GetAccountService() service.AccountServicesService {
+	accountOnce.Do(func() {
+		options := getClientOptions()
+		accountService = service.NewAccountServicesService(AccountService, grpcclient.NewClient(options...))
+	})
+
+	return accountService
+}
