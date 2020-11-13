9867838aa8db9ddde7d69119fb1467a1f2d8b034
Adi Hermawan <adidot78@gmail.com>
2020-11-13 18:21:44 +0700

    Add Organization Schema Endpoint

--- api/graph/resolver/spectra.resolvers.go
+++ api/graph/resolver/spectra.resolvers.go
@@ -10,27 +10,19 @@ import (
 	"github.com/skinnyguy/spectra-services/api/graph/model"
 )
 
-func (r *mutationResolver) Account(ctx context.Context) (*model.AbstractModel, error) {
-	return new(model.AbstractModel), nil
-}
-
-func (r *mutationResolver) Family(ctx context.Context) (*model.AbstractModel, error) {
-	return new(model.AbstractModel), nil
-}
-
 func (r *mutationResolver) Privileges(ctx context.Context) (*model.AbstractModel, error) {
 	return new(model.AbstractModel), nil
 }
 
-func (r *queryResolver) Account(ctx context.Context) (*model.AbstractModel, error) {
-	return new(model.AbstractModel), nil
-}
-
-func (r *queryResolver) Family(ctx context.Context) (*model.AbstractModel, error) {
+func (r *mutationResolver) Organization(ctx context.Context) (*model.AbstractModel, error) {
 	return new(model.AbstractModel), nil
 }
 
 func (r *queryResolver) Privileges(ctx context.Context) (*model.AbstractModel, error) {
+	return new(model.AbstractModel), nil
+}
+
+func (r *queryResolver) Organization(ctx context.Context) (*model.AbstractModel, error) {
 	return new(model.AbstractModel), nil
 }
 
@@ -42,16 +34,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) Role(ctx context.Context) (*model.AbstractModel, error) {
-	return new(model.AbstractModel), nil
-}
-func (r *queryResolver) Role(ctx context.Context) (*model.AbstractModel, error) {
-	return new(model.AbstractModel), nil
-}
